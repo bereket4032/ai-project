33de25538e418e89a873725b79df6e0c3c00b4c9
bereket4032 <bereketendalebekele@gmail.com>
2025-09-03 13:02:03 +0300

    Add Vercel serverless backend and homepage

--- api/index.js
+++
@@ -1,6 +0,0 @@
-// api/index.js
-import { json } from 'micro';
-
-export default async function handler(req, res) {
-  res.status(200).send("Hello AI World! 🚀 This is your live serverless endpoint.");
-}
